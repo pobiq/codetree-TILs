a5a6fafea17b14c5b61b6e2a0871c0c0519a3909
junghoon <111417407+pobiq@users.noreply.github.com>
2024-02-27 16:38:50 +0900

    Title: 최고의 33위치, Time: 100ms, Memory: 9MB, Status: Passed - CodeTree

--- 240227/최고의 33위치/best-place-of-33.java
+++ 240227/최고의 33위치/best-place-of-33.java
@@ -1,5 +1,41 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuffer sb = new StringBuffer();
+        
+        int n = Integer.parseInt(br.readLine());
+        int answer = 0;
+        int[][] map = new int[n][n];
+
+        for(int i=0; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i=0; i<=n-3; i++) {
+            for(int j=0; j<=n-3; j++) {
+                int count = 0;
+                for(int a=i; a<i+3; a++) {
+                    for(int b=j; b<j+3; b++) {
+                        if(map[a][b] == 1) {
+                            count++;
+                        }
+                    }
+                }
+                answer = Math.max(answer, count);
+            }
+        }
+
+        sb.append(answer);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
