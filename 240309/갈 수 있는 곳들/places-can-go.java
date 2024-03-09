dbb3b947daf78e15ffa9c875cc2ab9bbec50d2fd
junghoon <111417407+pobiq@users.noreply.github.com>
2024-03-09 19:28:48 +0900

    Title: 갈 수 있는 곳들, Time: 346ms, Memory: 31MB, Status: Wrong Answer - CodeTree

--- 240309/갈 수 있는 곳들/places-can-go.java
+++ 240309/갈 수 있는 곳들/places-can-go.java
@@ -36,18 +36,15 @@ public class Main {
             int c = Integer.parseInt(st2.nextToken()) - 1;
             bfs(i, r, c);
         }
-
-
-
+        
+        
         for(int a = 0; a < n; a++) {
             for(int b = 0; b < n; b++) {
                 boolean flag = true;
                 for(int i = 0; i < k; i++) {
-                    if (map[a][b] == 0) {
-                        if (!visited[i][a][b]) {
-                            flag = false;
-                            break;
-                        }
+                    if (!visited[i][a][b]) {
+                        flag = false;
+                        break;
                     }
                 }
                 if(flag) {
@@ -71,8 +68,8 @@ public class Main {
             int[] temp = queue.poll();
 
             for(int a = 0; a < 4; a++) {
-                int nextX = temp[0] + dx[a];
-                int nextY = temp[1] + dy[a];
+                int nextX = temp[1] + dx[a];
+                int nextY = temp[2] + dy[a];
 
                 if(nextX < 0 || nextX >= n || nextY < 0 || nextY >= n) continue;
 
