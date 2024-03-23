aa51f4266c36c28ba8f8542886d19b771d9dc14f
junghoon <111417407+pobiq@users.noreply.github.com>
2024-03-23 20:52:19 +0900

    Title: k개의 벽 없애기, Time: 102ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240323/k개의 벽 없애기/remove-k-walls.java
@@ -0,0 +1,109 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int n, k;
+    static int r1,c1,r2,c2;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[][] dist;
+
+    static public class Node {
+        int x;
+        int y;
+        int count;
+
+        public Node(int x, int y, int count) {
+            this.x = x;
+            this.y = y;
+            this.count = count;
+        }
+
+        public String toString() {
+            return this.x + " " + this.y;
+        }
+    }
+
+    static int result = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+
+        StringBuffer sb = new StringBuffer();
+
+        map = new int[n][n];
+        visited = new boolean[n][n];
+        dist = new int[n][n];
+
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st1.nextToken());
+            }
+        }
+
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        r1 = Integer.parseInt(st2.nextToken()) - 1;
+        c1 = Integer.parseInt(st2.nextToken()) - 1;
+        StringTokenizer st3 = new StringTokenizer(br.readLine());
+        r2 = Integer.parseInt(st3.nextToken()) - 1;
+        c2 = Integer.parseInt(st3.nextToken()) - 1;
+
+        bfs(new Node(r1, c1, 0));
+
+        if(dist[r2][c2] == 0) {
+            result = -1;
+        } else {
+            result = dist[r2][c2];
+        }
+
+        sb.append(result);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static void bfs(Node temp) {
+        visited[temp.x][temp.y] = true;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(temp);
+
+        while(!queue.isEmpty()) {
+            Node node = queue.poll();
+            int x = node.x;
+            int y = node.y;
+            int count = node.count;
+
+            for(int i = 0; i < 4; i++) {
+                int nextX = x + dx[i];
+                int nextY = y + dy[i];
+
+                if(nextX < 0 || nextX >= n || nextY < 0 || nextY >= n) continue;
+
+                if(visited[nextX][nextY]) continue;
+
+                if(map[nextX][nextY] == 1) {
+                    if(count < k) {
+                        visited[nextX][nextY] = true;
+                        dist[nextX][nextY] = dist[x][y] + 1;
+                        queue.add(new Node(nextX, nextY, count+1));
+                    }
+                } else {
+                    visited[nextX][nextY] = true;
+                    dist[nextX][nextY] = dist[x][y] + 1;
+                    queue.add(new Node(nextX, nextY, count));
+                }
+            }
+        }
+    }
+}
