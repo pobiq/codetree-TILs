ac5db2eab616e89869eb1b5bf0b3197f76394e26
junghoon <111417407+pobiq@users.noreply.github.com>
2024-04-02 15:49:17 +0900

    Title: 사각형 채우기 2, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240402/사각형 채우기 2/rectangle-fill-2.java
+++ 240402/사각형 채우기 2/rectangle-fill-2.java
@@ -17,7 +17,7 @@ public class Main {
         dp[2] = 3;
 
         for(int i = 3; i <= n; i++) {
-            dp[i] = (dp[i-1] + dp[i-2] + dp[i-3]) % mod;
+            dp[i] = (dp[i-1] * 2 + dp[i-2]) % mod;
         }
 
         sb.append(dp[n]);
