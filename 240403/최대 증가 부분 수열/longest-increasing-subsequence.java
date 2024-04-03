3df8885d67e1ac034942df7f55b00ebec96bb920
junghoon <111417407+pobiq@users.noreply.github.com>
2024-04-03 16:18:46 +0900

    Title: 최대 증가 부분 수열, Time: 147ms, Memory: 9MB, Status: Passed - CodeTree

--- 240403/최대 증가 부분 수열/longest-increasing-subsequence.java
+++ 240403/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -10,19 +10,20 @@ public class Main {
 
         n = Integer.parseInt(br.readLine());
 
-        int[] map = new int[n];
-        int[] dp = new int[n];
+        int[] map = new int[n + 1];
+        int[] dp = new int[n + 1];
         int result = 0;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++) {
+        for(int i = 1; i <= n; i++) {
             map[i] = Integer.parseInt(st.nextToken());
             dp[i] = Integer.MIN_VALUE;
         }
 
-        dp[0] = 1;
+        dp[0] = 0;
+        map[0] = 0;
 
-        for(int i = 1; i < n; i++) {
+        for(int i = 1; i <= n; i++) {
             for(int j = 0; j < i; j++) {
                 if(map[i] > map[j]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
@@ -30,7 +31,7 @@ public class Main {
             }
         }
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i <= n; i++) {
             result = Math.max(result, dp[i]);
         }
 
