70cb7bf91987570b718a9019466227b5f2cd70bc
junghoon <111417407+pobiq@users.noreply.github.com>
2024-04-03 16:21:03 +0900

    Title: 최대 감소 부분 수열, Time: 138ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240403/최대 감소 부분 수열/longest-decreasing-subsequence.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuffer sb = new StringBuffer();
+
+        n = Integer.parseInt(br.readLine());
+
+        int[] map = new int[n + 1];
+        int[] dp = new int[n + 1];
+        int result = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= n; i++) {
+            map[i] = Integer.parseInt(st.nextToken());
+            dp[i] = Integer.MIN_VALUE;
+        }
+
+        map[0] = Integer.MAX_VALUE;
+        dp[0] = 0;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(map[i] < map[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        for(int i = 0; i <= n; i++) {
+            result = Math.max(result, dp[i]);
+        }
+
+        sb.append(result);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+
+}
